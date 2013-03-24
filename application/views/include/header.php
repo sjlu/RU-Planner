9850860b216029de6a98732661801aa150fbd111
Steven Lu <tacticalazn@gmail.com>
2013-03-23 22:54:58 -0400

    Re-tab.

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -1,18 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta name="description" content="RU Planner helps your plan out your engineering courses at Rutgers." />
-   <meta name="keywords" content="engineering, classes, rutgers, courses, planner" />
+	<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="RU Planner helps your plan out your engineering courses at Rutgers." />
+	<meta name="keywords" content="engineering, classes, rutgers, courses, planner" />
 
-   <title>RU Planner</title>
+	<title>RU Planner</title>
 
-   <link href="<?= base_url('assets/style.css') ?>" rel="stylesheet">
+	<link href="<?= base_url('assets/style.css') ?>" rel="stylesheet">
 
-   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
 	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
-   <script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/0.9.0/lodash.min.js"></script>
+	<script src="//cdnjs.cloudflare.com/ajax/libs/lodash.js/0.9.0/lodash.min.js"></script>
 	<script src="<?= base_url('assets/app.js') ?>"></script>
 </head>
 <body>
