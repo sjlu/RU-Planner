858c36cf64f1a2919a311da946b8e301cd764c59
Steven Lu <tacticalazn@gmail.com>
2013-04-14 00:26:59 -0400

    Added listing of courses.

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,19 @@
-<?php
+<div class="container">
+	<table class="table table-bordered table-striped">
+		<thead>
+			<tr>
+				<th>Course</th>
+				<th>Name</th>
+			</tr>
+		</thead>
+		<tbody>
+			<? foreach($major_courses as $course): ?>
+				<tr>
+					<td><?= $course->school ?>:<?= $course->course ?></a></td>
+					<td><?= $course->name ?></td>
+				</tr>
+			<? endforeach; ?>
+		</tbody>
+	</table>
 
-print_r($major_courses);
+</div>
